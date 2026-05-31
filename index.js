612ef9506ee52f64530fd4008f811c6a0ecd6a1d
Sofia <sofia.demarco.en@gmail.com>
2026-05-31 12:07:02 -0700

    challenge 6

--- index.js
+++ index.js
@@ -144,12 +144,20 @@ allBtn.addEventListener("click", showAllGames);
 const descriptionContainer = document.getElementById("description-container");
 
 // use filter or reduce to count the number of unfunded games
-
+let unmetGoal = GAMES_JSON.filter( (game) => {
+    return game.goal > game.pledged;
+});
+let unmetNum = unmetGoal.length;
 
 // create a string that explains the number of unfunded games using the ternary operator
-
+let unfundedStr = `A total of $${totRaised.toLocaleString('en-US')} has been raised for ${GAMES_JSON.length} games.
+    Currently, ${unmetNum} ${unmetNum === 1 ? "game remains" : "games remain"} unfunded. We need
+    your help to fund these amazing games!`;
 
 // create a new DOM element containing the template string and append it to the description container
+const templateStr = document.createElement("p");
+templateStr.innerHTML = `${unfundedStr}`;
+descriptionContainer.appendChild(templateStr);
 
 /************************************************************************************
  * Challenge 7: Select & display the top 2 games
